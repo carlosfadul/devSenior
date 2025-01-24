abcc70a95dd3f69ce76b039b620c5021e0834642
carlosfadul <carlosfadul@gmail.com>
2025-01-23 21:20:53 -0500

    agrege primero.py y tuplas.py al repo local

---
+++ primero.py
@@ -0,0 +1,9 @@
+a = 5
+b = 6 
+c = 3
+
+resultado = (a < b) and (c >= a) or (5 == 4)
+
+print(resultado)
+
+
---
+++ tuplas.py
@@ -0,0 +1,6 @@
+frutas = ("papaya","manzana","naranja","mandarina")
+#frutas2 = tuple(("papaya","manzana","naranja","mandarina"))
+print(frutas)
+
+print(frutas.count("naranja"))
+print(frutas.index("naranja"))
